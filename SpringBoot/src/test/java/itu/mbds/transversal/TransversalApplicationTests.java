d29cdf981060a03655457273f9b0db2291ecde88
FelanaRasata <77356909+FelanaRasata@users.noreply.github.com>
2024-07-10 10:27:54 +0300

    Init Spring boot

---
+++ SpringBoot/src/test/java/itu/mbds/transversal/TransversalApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.mbds.transversal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransversalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
